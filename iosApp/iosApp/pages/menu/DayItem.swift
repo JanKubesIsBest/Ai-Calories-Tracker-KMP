793de51516697926f201dd2f691c2fdaf89436ac
JanKubesIsBest <jan.kubes07@gmail.com>
2025-03-18 22:38:25 +0100

    feat(iOS): Blur!!!!

--- iosApp/iosApp/pages/menu/DayItem.swift
+++ iosApp/iosApp/pages/menu/DayItem.swift
@@ -18,20 +18,30 @@ struct NormalDay: View {
     
     var body: some View {
         NavigationLink(destination: DayMealsView(date: day.date)) {
-            // If it's the first item (index == 0), make a bigger card
-            // Smaller card for the rest
-            VStack(alignment: .leading, spacing: 8) {
-                Text(day.title)
-                    .font(.headline)
-                Text(day.description_)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-                + Text(" Total calories: \(day.totalCalories)")
-                    .font(.subheadline)
-                    .bold()
+            HStack {
+                VStack (alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        Text(day.title)
+                            .font(.headline)
+                        Text(day.description_)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                        + Text(" Total calories: \(day.totalCalories)")
+                            .font(.subheadline)
+                            .bold()
+                        
+                    }
+                    .multilineTextAlignment(.leading)
+                    
+                    
+                    Divider()
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
             }
         }
-
+        .padding(.vertical, 8)
+        .frame(width: 350)
     }
 }
 
@@ -44,34 +54,45 @@ struct TodayDay: View {
     
     var body: some View {
         NavigationLink(destination: DayMealsView(date: day.date)) {
-            VStack(alignment: .leading, spacing: 8) {
-                Text(day.title)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.top, 5)
-                Text(day.description_)
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-                + Text(" Total calories: \(day.totalCalories)")
-                    .font(.subheadline)
-                    .bold()
-                
-                if #available(iOS 16.0, *) {
-                    Chart {
-                        ForEach(MealsInDayViewModel.companion.groupMealsByTimeDifference(meals: day.meals), id: \.self) { section in
-                            BarMark(
-                                x: .value("Time", section.sectionName),
-                                y: .value("Total Calories", section.totalCalories())
-                            )
-                        }
-                    }.padding(.top, 10)
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(day.title)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.top, 5)
+                    Text(day.description_)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    + Text(" Total calories: \(day.totalCalories)")
+                        .font(.subheadline)
+                        .bold()
+                    
+                    if #available(iOS 16.0, *) {
+                        Chart {
+                            ForEach(MealsInDayViewModel.companion.groupMealsByTimeDifference(meals: day.meals), id: \.self) { section in
+                                BarMark(
+                                    x: .value("Time", section.sectionName),
+                                    y: .value("Total Calories", section.totalCalories())
+                                )
+                            }
+                        }.padding(.top, 10)
+                    }
+                    
+                    Divider()
                 }
+                .multilineTextAlignment(.leading)
+                .frame(
+                    minHeight: 250,
+                    alignment: .topLeading
+                )
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
             }
-            .frame(
-                maxWidth: .infinity,
-                minHeight: 250,
-                alignment: .topLeading
-            )
         }
+        .padding(.vertical, 8)
+        .frame(width: 350)
+        .padding(.horizontal, 15)
     }
 }
--- iosApp/iosApp/pages/menu/Menu.swift
+++ iosApp/iosApp/pages/menu/Menu.swift
@@ -9,78 +9,60 @@ import SwiftUI
 import Shared
 
 struct MenuView: View {
-    
     @State private var viewModel: MenuViewModel
     private let db: Database
     
     init() {
         let driverFactory = DriverFactory()
         self.db = Database(databaseDriverFactory: driverFactory)
-        
         self._viewModel = State(initialValue: MenuViewModel(database: db))
     }
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                LinearGradient(
-                    gradient: Gradient(colors: [Color.blue.opacity(0.5), Color.white]),
-                    startPoint: .top,
-                    endPoint: .bottom
-                )
-                .clipShape(LeftRoundedRectangle(radius: 20))
-                .edgesIgnoringSafeArea(.all)
+        GeometryReader { geometry in
+            NavigationView {
                 
+                // Scrollable content
                 if #available(iOS 17.0, *) {
-                    List {
-                        Observing(viewModel.menuState) { state in
-                            TodayDay(day: state.days[0])
-                            
-                            ForEach(1..<state.days.count, id: \.self) { index in
-                                NormalDay(day: state.days[index])
+                        // Background image as a separate layer
+                        
+                            ScrollView {
+                                ZStack {
+                                    Image("eclipse")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 800, height: 800)
+                                        .offset(x: -120, y: -700)
+                                        .edgesIgnoringSafeArea(.all)
+                                    
+                                    VStack {
+                                        Observing(viewModel.menuState) { state in
+
+                                                
+                                            TodayDay(day: state.days[0])
+
+                                            
+                                            ForEach(1..<state.days.count, id: \.self) { index in
+                                                    NormalDay(day: state.days[index])
+                                                
+                                            }
+                                        }
+                                    }
+                                    .background(Color.white)
+                                    .cornerRadius(10)
+                                    .padding(10)
+                                    .padding(.horizontal, geometry.safeAreaInsets.leading)
+                                    .frame(minWidth: 400)
+                                    .navigationTitle("Menu")
+                                }
                             }
-                        }
-                    }
-                    .listStyle(.plain)
-                    .scrollContentBackground(.hidden)
+                    
+                    
                 } else {
-                    // Fallback
+                    // Fallback on earlier versions
                 }
             }
-            .navigationTitle("Menu")
-        }
-    }
-}
 
-struct LeftRoundedRectangle: Shape {
-    var radius: CGFloat
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        let width = rect.width
-        let height = rect.height
-        
-        path.move(to: CGPoint(x: radius, y: 0))
-        path.addLine(to: CGPoint(x: width, y: 0))
-        path.addLine(to: CGPoint(x: width, y: height))
-        path.addLine(to: CGPoint(x: radius, y: height))
-        path.addArc(
-            center: CGPoint(x: radius, y: height - radius),
-            radius: radius,
-            startAngle: .degrees(90),
-            endAngle: .degrees(180),
-            clockwise: false
-        )
-        path.addLine(to: CGPoint(x: 0, y: radius))
-        path.addArc(
-            center: CGPoint(x: radius, y: radius),
-            radius: radius,
-            startAngle: .degrees(180),
-            endAngle: .degrees(270),
-            clockwise: false
-        )
-        path.closeSubpath()
-        
-        return path
+        }
     }
 }
